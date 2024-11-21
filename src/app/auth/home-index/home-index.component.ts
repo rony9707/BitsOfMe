b1451110c507afc12d7fca16b0eb583594895c97
Rony <50582705+rony9707@users.noreply.github.com>
2024-11-21 20:48:31 +0530

    release-3

--- src/app/auth/home-index/home-index.component.ts
+++ src/app/auth/home-index/home-index.component.ts
@@ -3,12 +3,11 @@ import { DividerModule } from 'primeng/divider';
 import { NavButtonsComponent } from '../../shared/header/nav-buttons/nav-buttons.component';
 import { PostButtonsComponent } from '../../shared/header/post-buttons/post-buttons.component';
 import { RouterOutlet } from '@angular/router';
-import { HideScrollDirective } from '../../directive/hide-scroll/hide-scroll.directive';
 
 @Component({
   selector: 'app-home-index',
   standalone: true,
-  imports: [NavButtonsComponent,PostButtonsComponent,DividerModule,RouterOutlet,HideScrollDirective],
+  imports: [NavButtonsComponent,PostButtonsComponent,DividerModule,RouterOutlet],
   templateUrl: './home-index.component.html',
   styleUrl: './home-index.component.css'
 })
