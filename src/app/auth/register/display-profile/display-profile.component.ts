35b68d0d29bc0575ed7b4c7f68a108762fe87847
Rony <50582705+rony9707@users.noreply.github.com>
2024-12-03 23:10:43 +0530

    RegistrationPage-Release 1

--- src/app/auth/register/display-profile/display-profile.component.ts
+++ src/app/auth/register/display-profile/display-profile.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, inject, Input, OnInit, Output, signal, WritableSignal } from '@angular/core';
+import { registerFormInterface } from '../register-form.interface';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-display-profile',
   standalone: true,
-  imports: [],
+  imports: [DividerModule],
   templateUrl: './display-profile.component.html',
   styleUrl: './display-profile.component.css'
 })
 export class DisplayProfileComponent {
+
+  @Input() registerFormData!: registerFormInterface;
+  @Input() uploadedImage: string | ArrayBuffer | null = null;
+  displayProfileFlag = false
+  windowWidth: WritableSignal<number> = signal(0);
+
+
+  //Inject Services here------------------------------------------------------
+
+
+
+  constructor(){
+    this.windowWidth.set(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.windowWidth.set(window.innerWidth);
+  }
+
+  get firstName(): string {
+    return this.registerFormData?.fullname?.split(' ')[0]?.toUpperCase() || '';
+  }
+  
+  get lastName(): string {
+    return this.registerFormData?.fullname?.split(' ')?.slice(1)?.join(' ') || '';
+  }
+
+
 
 }
---
+++ src/app/auth/register/register-form.interface.ts
@@ -0,0 +1,13 @@
+export interface registerFormInterface {
+  username: string;
+  password:string;
+  uploadImage:string;
+  fullname:string;
+  dob:string;
+  gender:string;
+  aboutme:string;
+  email:string;
+  countryCode:string;
+  phoneNumber:string;
+  address:string;
+}
